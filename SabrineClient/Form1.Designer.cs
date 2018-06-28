eb83bd4b4a0b5facfb28ca08651e6387af694409
Geo <ravenholmgeorge@gmail.com>
2018-06-28 17:43:49 +1000

    cl send window doesnt appear on taskbar

--- SabrineClient/Form1.Designer.cs
+++ SabrineClient/Form1.Designer.cs
@@ -28,44 +28,46 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.tMessage = new System.Windows.Forms.TextBox();
-			this.Stend = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// tMessage
-			// 
-			this.tMessage.Location = new System.Drawing.Point(13, 6);
-			this.tMessage.Name = "tMessage";
-			this.tMessage.Size = new System.Drawing.Size(283, 20);
-			this.tMessage.TabIndex = 0;
-			this.tMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tMessage_KeyDown);
-			// 
-			// Stend
-			// 
-			this.Stend.Location = new System.Drawing.Point(303, 6);
-			this.Stend.Name = "Stend";
-			this.Stend.Size = new System.Drawing.Size(81, 23);
-			this.Stend.TabIndex = 1;
-			this.Stend.Text = "Stend";
-			this.Stend.UseVisualStyleBackColor = true;
-			this.Stend.Click += new System.EventHandler(this.Stend_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(396, 38);
-			this.ControlBox = false;
-			this.Controls.Add(this.Stend);
-			this.Controls.Add(this.tMessage);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "Form1";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "TrapChat";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.tMessage = new System.Windows.Forms.TextBox();
+            this.Stend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tMessage
+            // 
+            this.tMessage.Location = new System.Drawing.Point(13, 6);
+            this.tMessage.Name = "tMessage";
+            this.tMessage.Size = new System.Drawing.Size(283, 20);
+            this.tMessage.TabIndex = 0;
+            this.tMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tMessage_KeyDown);
+            // 
+            // Stend
+            // 
+            this.Stend.Location = new System.Drawing.Point(303, 6);
+            this.Stend.Name = "Stend";
+            this.Stend.Size = new System.Drawing.Size(81, 23);
+            this.Stend.TabIndex = 1;
+            this.Stend.Text = "Stend";
+            this.Stend.UseVisualStyleBackColor = true;
+            this.Stend.Click += new System.EventHandler(this.Stend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 38);
+            this.ControlBox = false;
+            this.Controls.Add(this.Stend);
+            this.Controls.Add(this.tMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrapChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
