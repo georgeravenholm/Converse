c1f065d98a6e8abe242e59dc01df9169e8b25449
Geo <ravenholmgeorge@gmail.com>
2018-06-30 12:00:18 +1000

    make typing box centreparent instead of centrescreen

--- SabrineClient/Form1.Designer.cs
+++ SabrineClient/Form1.Designer.cs
@@ -64,7 +64,7 @@
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "TrapChat";
             this.ResumeLayout(false);
             this.PerformLayout();
